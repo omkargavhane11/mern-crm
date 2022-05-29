3d994ce28627b6c860c848061322dd2eaa1b6128
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-05-29 18:04:10 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { AddLead } from './AddLead';
 import { AddService } from './AddService';
 import { Home } from './Home';
 import { SignIn } from './SignIn';
+import { EditLead } from './EditLead';
+import { EditService } from './EditService';
 
 function App() {
 
@@ -22,6 +24,8 @@ function App() {
       username: "username1",
       address: "address1",
       contactNo: 9012345678,
+      intrest: "something",
+      status: "new"
     },
     {
       id: 2,
@@ -32,6 +36,8 @@ function App() {
       username: "username2",
       address: "address2",
       contactNo: 9012345678,
+      intrest: "something",
+      status: "new"
     },
     {
       id: 3,
@@ -42,6 +48,8 @@ function App() {
       username: "username3",
       address: "address3",
       contactNo: 9012345678,
+      intrest: "something",
+      status: "new"
     },
   ]);
 
@@ -54,6 +62,9 @@ function App() {
       password: "password1",
       username: "username1",
       address: "address1",
+      contactNo: 9191919191,
+      intrest: "something",
+      status: "new"
     },
     {
       id: 2,
@@ -63,6 +74,9 @@ function App() {
       password: "password2",
       username: "username2",
       address: "address2",
+      contactNo: 9191919191,
+      intrest: "something",
+      status: "new"
     },
     {
       id: 3,
@@ -72,47 +86,58 @@ function App() {
       password: "password3",
       username: "username3",
       address: "address3",
+      contactNo: 9191919191,
+      intrest: "something",
+      status: "new"
     },
   ]);
 
-  const [users, setUsers] = [
-    {
-      fname: "omkar",
-      lname: "gavhane",
-      email: "ogomkargavhane@gmail.com",
-      password: "let@123",
-      username: "omkar123",
-      contact: 9191919191
-    }
-  ]
+  const [users, setUsers] = useState(
+    [
+      {
+        fname: "omkar",
+        lname: "gavhane",
+        username: "omkar123",
+        email: "ogomkargavhane@gmail.com",
+        password: "let@123",
+        contact: 9082732814,
+        role: "admin"
+      },
+    ]
+  );
+
 
   const navigate = useNavigate();
   return (
     <div className="App">
       <div className="navbar">
         <img src="" alt="loading" />
-        {/* <h5 className="navbar_link" onClick={() => navigate('/')}>Company</h5> */}
+        <h5 className="navbar_link" onClick={() => navigate('/')}>Company</h5>
         <h5 className="navbar_link home_navlink" onClick={() => navigate('/home')}>Home</h5>
         <h5 className="navbar_link" onClick={() => navigate('/leads')}>Leads</h5>
         <h5 className="navbar_link" onClick={() => navigate('/services')}>Services</h5>
         <h5 className="navbar_link" onClick={() => navigate('/contacts')}>Contacts</h5>
         <h5 className="navbar_link logout" onClick={() => navigate('/')}>Log Out</h5>
       </div>
-      <div className="main">
+      <div div className="main" >
         <Routes>
-          <Route path="/" element={<SignIn users={users} setUsers={setUsers} />} />
+          <Route path="/" element={<SignIn users={users} setUsers={setUsers} lead_data={lead_data} />} />
           <Route path="/signup" element={<Signup users={users} setUsers={setUsers} />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<Home lead_data={lead_data} service_data={service_data} />} />
           <Route path="/leads" element={<Leads lead_data={lead_data} setlead_data={setlead_data} />} />
+          <Route path="/leads/edit/:id" element={<EditLead lead_data={lead_data} setlead_data={setlead_data} />} />
           <Route path="/leads/add_lead_request" element={<AddLead lead_data={lead_data} setlead_data={setlead_data} />} />
           <Route path="/services" element={<Services service_data={service_data} setservice_data={setservice_data} />} />
+          <Route path="/services/edit/:id" element={<EditService service_data={service_data} setservice_data={setservice_data} />} />
           <Route path="/services/new_service_request" element={<AddService service_data={service_data} setservice_data={setservice_data} />} />
         </Routes>
-      </div>
-    </div>
+
+      </div >
+    </div >
   );
 }
 
 export default App;
+
 
 
