a452738dade1f530e7b16df712ced9fb36f5ca43
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-05-31 08:25:11 +0530

    navbar quick fix

--- src/AddLead.js
+++ src/AddLead.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { API } from "./global.js";
 import React from 'react';
 import { send } from 'emailjs-com';
+import { Navbar } from "./Navbar";
 
 export function AddLead() {
 
@@ -28,57 +29,60 @@ export function AddLead() {
     // };
 
     return (
-        <div className="addLead_parent">
+        <>
+            <Navbar />
+            <div className="addLead_parent">
 
-            <div className="addLead_form">
-                <h3>Add New Lead </h3>
-                <label className="addLead_label" for="fname">First Name</label>
-                <input onChange={(e) => setFname(e.target.value)} className="addLead_input" type="text" id="fname" name="firstname" placeholder="Your firstname.." />
+                <div className="addLead_form">
+                    <h3>Add New Lead </h3>
+                    <label className="addLead_label" for="fname">First Name</label>
+                    <input onChange={(e) => setFname(e.target.value)} className="addLead_input" type="text" id="fname" name="firstname" placeholder="Your firstname.." />
 
-                <label className="addLead_label" for="lname">Last Name</label>
-                <input onChange={(e) => setLname(e.target.value)} className="addLead_input" type="text" id="lname" name="lastname" placeholder="Your last name.." />
+                    <label className="addLead_label" for="lname">Last Name</label>
+                    <input onChange={(e) => setLname(e.target.value)} className="addLead_input" type="text" id="lname" name="lastname" placeholder="Your last name.." />
 
-                <label className="addLead_label" for="Email">Email</label>
-                <input onChange={(e) => setEmail(e.target.value)} className="addLead_input" type="email" id="Email" name="Email" placeholder="Your email.." />
+                    <label className="addLead_label" for="Email">Email</label>
+                    <input onChange={(e) => setEmail(e.target.value)} className="addLead_input" type="email" id="Email" name="Email" placeholder="Your email.." />
 
-                <label className="addLead_label" for="Address">Address</label>
-                <input onChange={(e) => setAddress(e.target.value)} className="addLead_input" type="text" id="Address" name="Address" placeholder="Address" />
+                    <label className="addLead_label" for="Address">Address</label>
+                    <input onChange={(e) => setAddress(e.target.value)} className="addLead_input" type="text" id="Address" name="Address" placeholder="Address" />
 
-                <label className="addLead_label" for="contactNo">Contact No</label>
-                <input onChange={(e) => setContact(e.target.value)} className="addLead_input" type="number" id="contactNo" name="contactNo" placeholder="contactNo" />
+                    <label className="addLead_label" for="contactNo">Contact No</label>
+                    <input onChange={(e) => setContact(e.target.value)} className="addLead_input" type="number" id="contactNo" name="contactNo" placeholder="contactNo" />
 
-                <label className="addLead_label" for="intrest">Intrested in </label>
-                <input onChange={(e) => setIntrest(e.target.value)} className="addLead_input" type="text" id="intrest" name="intrest" placeholder="intrest" />
-
-
-                <button
-                    onClick={() => {
-                        let newLead = {
-                            fname: fname,
-                            lname: lname,
-                            email: email,
-                            address: address,
-                            contactNo: contact,
-                            intrest: intrest,
-                            status: "new"
-                        }
-                        fetch(`${API}/leads`, {
-                            method: "POST",
-                            body: JSON.stringify(newLead),
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                        }).then((data) => data.json())
-                            .then(() => navigate(`/${username}/leads`))
-
-                        // sendEmail(newLead)
-                    }}
+                    <label className="addLead_label" for="intrest">Intrested in </label>
+                    <input onChange={(e) => setIntrest(e.target.value)} className="addLead_input" type="text" id="intrest" name="intrest" placeholder="intrest" />
 
 
-                    className="addLead_form_save" type="submit" value="Submit">Add</button>
-            </div>
+                    <button
+                        onClick={() => {
+                            let newLead = {
+                                fname: fname,
+                                lname: lname,
+                                email: email,
+                                address: address,
+                                contactNo: contact,
+                                intrest: intrest,
+                                status: "new"
+                            }
+                            fetch(`${API}/leads`, {
+                                method: "POST",
+                                body: JSON.stringify(newLead),
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                            }).then((data) => data.json())
+                                .then(() => navigate(`/${username}/leads`))
+
+                            // sendEmail(newLead)
+                        }}
 
 
-        </div >
+                        className="addLead_form_save" type="submit" value="Submit">Add</button>
+                </div>
+
+
+            </div >
+        </>
     );
 }
--- src/AddService.js
+++ src/AddService.js
@@ -2,7 +2,8 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useState } from 'react';
 import { API } from "./global.js";
 import React from 'react';
-import { send } from 'emailjs-com';
+// import { send } from 'emailjs-com';
+import { Navbar } from "./Navbar";
 
 export function AddService() {
 
@@ -26,57 +27,59 @@ export function AddService() {
     // };
 
     return (
+        <>
+            <Navbar />
+            <div className="addLead_parent">
 
-        <div className="addLead_parent">
+                <div className="addLead_form">
+                    <h3>  New Service Request </h3>
+                    <label className="addLead_label" for="fname">First Name</label>
+                    <input onChange={(e) => setFname(e.target.value)} className="addLead_input" type="text" id="fname" name="firstname" placeholder="Your firstname.." />
 
-            <div className="addLead_form">
-                <h3>  New Service Request </h3>
-                <label className="addLead_label" for="fname">First Name</label>
-                <input onChange={(e) => setFname(e.target.value)} className="addLead_input" type="text" id="fname" name="firstname" placeholder="Your firstname.." />
+                    <label className="addLead_label" for="lname">Last Name</label>
+                    <input onChange={(e) => setLname(e.target.value)} className="addLead_input" type="text" id="lname" name="lastname" placeholder="Your last name.." />
 
-                <label className="addLead_label" for="lname">Last Name</label>
-                <input onChange={(e) => setLname(e.target.value)} className="addLead_input" type="text" id="lname" name="lastname" placeholder="Your last name.." />
+                    <label className="addLead_label" for="Email">Email</label>
+                    <input onChange={(e) => setEmail(e.target.value)} className="addLead_input" type="email" id="Email" name="Email" placeholder="Your email.." />
 
-                <label className="addLead_label" for="Email">Email</label>
-                <input onChange={(e) => setEmail(e.target.value)} className="addLead_input" type="email" id="Email" name="Email" placeholder="Your email.." />
+                    <label className="addLead_label" for="Address">Address</label>
+                    <input onChange={(e) => setAddress(e.target.value)} className="addLead_input" type="text" id="Address" name="Address" placeholder="Address" />
 
-                <label className="addLead_label" for="Address">Address</label>
-                <input onChange={(e) => setAddress(e.target.value)} className="addLead_input" type="text" id="Address" name="Address" placeholder="Address" />
+                    <label className="addLead_label" for="contactNo">Contact No</label>
+                    <input onChange={(e) => setContact(e.target.value)} className="addLead_input" type="number" id="contactNo" name="contactNo" placeholder="contactNo" />
 
-                <label className="addLead_label" for="contactNo">Contact No</label>
-                <input onChange={(e) => setContact(e.target.value)} className="addLead_input" type="number" id="contactNo" name="contactNo" placeholder="contactNo" />
-
-                <label className="addLead_label" for="intrest">Intrested in </label>
-                <input onChange={(e) => setIntrest(e.target.value)} className="addLead_input" type="text" id="intrest" name="intrest" placeholder="intrest" />
+                    <label className="addLead_label" for="intrest">Intrested in </label>
+                    <input onChange={(e) => setIntrest(e.target.value)} className="addLead_input" type="text" id="intrest" name="intrest" placeholder="intrest" />
 
 
 
-                <button
-                    onClick={() => {
-                        let newService = {
-                            fname: fname,
-                            lname: lname,
-                            email: email,
-                            address: address,
-                            contactNo: contact,
-                            intrest: intrest,
-                            status: "new"
-                        }
-                        fetch(`${API}/services`, {
-                            method: "POST",
-                            body: JSON.stringify(newService),
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                        }).then((data) => data.json())
-                            .then(() => navigate(`/${username}/services`))
+                    <button
+                        onClick={() => {
+                            let newService = {
+                                fname: fname,
+                                lname: lname,
+                                email: email,
+                                address: address,
+                                contactNo: contact,
+                                intrest: intrest,
+                                status: "new"
+                            }
+                            fetch(`${API}/services`, {
+                                method: "POST",
+                                body: JSON.stringify(newService),
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                            }).then((data) => data.json())
+                                .then(() => navigate(`/${username}/services`))
 
-                        // sendEmail(newService)
-                    }}
-                    className="addLead_form_save" type="submit" value="Add">Save</button>
+                            // sendEmail(newService)
+                        }}
+                        className="addLead_form_save" type="submit" value="Add">Save</button>
+                </div>
+
+
             </div>
-
-
-        </div>
+        </>
     );
 }
