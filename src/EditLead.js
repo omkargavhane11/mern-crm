5e0538741a7e5003cb063e744266203659d1f187
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-06-14 02:27:42 +0530

    edit part ui improved

--- src/EditLead.js
+++ src/EditLead.js
@@ -2,6 +2,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import React from 'react';
 import { API } from "./global.js";
+import TextField from '@mui/material/TextField';
 
 export function EditLead() {
 
@@ -39,20 +40,13 @@ function EditLeadForm({ data }) {
         <div className="addLead_parent">
             <div className="addLead_form">
                 <h3>Edit Lead </h3>
-                <label className="addLead_label" htmlFor="fname">First Name</label>
-                <input onChange={(e) => setFname(e.target.value)} value={fname} className="addLead_input" type="text" id="fname" name="firstname" placeholder="Your firstname.." />
-                <label className="addLead_label" htmlFor="lname">Last Name</label>
-                <input onChange={(e) => setLname(e.target.value)} value={lname} className="addLead_input" type="text" id="lname" name="lastname" placeholder="Your last name.." />
-                <label className="addLead_label" htmlFor="Email">Email</label>
-                <input onChange={(e) => setEmail(e.target.value)} value={email} className="addLead_input" type="email" id="Email" name="Email" placeholder="Your address.." />
-                <label className="addLead_label" htmlFor="Address">Address</label>
-                <input onChange={(e) => setAddress(e.target.value)} value={address} className="addLead_input" type="text" id="Address" name="Address" placeholder="Address" />
-                <label className="addLead_label" htmlFor="contactNo">Contact No</label>
-                <input onChange={(e) => setContact(e.target.value)} value={contact} className="addLead_input" type="number" id="contactNo" name="contactNo" placeholder="contactNo" />
-                <label className="addLead_label" htmlFor="intrest">Intrested in </label>
-                <input onChange={(e) => setIntrest(e.target.value)} value={intrest} className="addLead_input" type="text" id="intrest" name="intrest" />
-                <label className="addLead_label" htmlFor="status">Lead Status</label>
-                <input onChange={(e) => setStatus(e.target.value)} value={status} className="addLead_input" type="text" id="status" name="status" placeholder="status" />
+                <TextField onChange={(e) => setFname(e.target.value)} value={fname} className="addLead_input" type="text" id="fname" label="firstname" placeholder="firstname.." />
+                <TextField onChange={(e) => setLname(e.target.value)} value={lname} className="addLead_input" type="text" id="lname" label="lastname" placeholder="last name.." />
+                <TextField onChange={(e) => setEmail(e.target.value)} value={email} className="addLead_input" type="email" id="Email" label="Email" placeholder="address.." />
+                <TextField onChange={(e) => setAddress(e.target.value)} value={address} className="addLead_input" type="text" id="Address" label="Address" placeholder="Address" />
+                <TextField onChange={(e) => setContact(e.target.value)} value={contact} className="addLead_input" type="number" id="contactNo" label="contactNo" placeholder="contactNo" />
+                <TextField onChange={(e) => setIntrest(e.target.value)} value={intrest} className="addLead_input" type="text" id="intrest" label="intrest" />
+                <TextField onChange={(e) => setStatus(e.target.value)} value={status} className="addLead_input" type="text" id="status" label="status" placeholder="status" />
 
 
                 <button
--- src/EditService.js
+++ src/EditService.js
@@ -2,6 +2,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import React from 'react';
 import { API } from "./global.js";
+import TextField from '@mui/material/TextField';
 
 export function EditService() {
 
@@ -35,20 +36,13 @@ function EditServiceForm({ data }) {
         <div className="addLead_parent">
             <div className="addLead_form">
                 <h3>Edit Service</h3>
-                <label className="addLead_label" for="fname">First Name</label>
-                <input onChange={(e) => setFname(e.target.value)} value={fname} className="addLead_input" type="text" id="fname" name="firstname" placeholder="Your firstname.." />
-                <label className="addLead_label" for="lname">Last Name</label>
-                <input onChange={(e) => setLname(e.target.value)} value={lname} className="addLead_input" type="text" id="lname" name="lastname" placeholder="Your last name.." />
-                <label className="addLead_label" for="Email">Email</label>
-                <input onChange={(e) => setEmail(e.target.value)} value={email} className="addLead_input" type="email" id="Email" name="Email" placeholder="Your address.." />
-                <label className="addLead_label" for="Address">Address</label>
-                <input onChange={(e) => setAddress(e.target.value)} value={address} className="addLead_input" type="text" id="Address" name="Address" placeholder="Address" />
-                <label className="addLead_label" for="contactNo">Contact No</label>
-                <input onChange={(e) => setContact(e.target.value)} value={contact} className="addLead_input" type="number" id="contactNo" name="contactNo" placeholder="contactNo" />
-                <label className="addLead_label" for="intrest">Intrested in </label>
-                <input onChange={(e) => setIntrest(e.target.value)} value={intrest} className="addLead_input" type="text" id="intrest" name="intrest" />
-                <label className="addLead_label" for="status">Lead Status</label>
-                <input onChange={(e) => setStatus(e.target.value)} value={status} className="addLead_input" type="text" id="status" name="status" placeholder="status" />
+                <TextField onChange={(e) => setFname(e.target.value)} value={fname} className="addLead_input" type="text" id="fname" label="firstname" placeholder="Your firstname.." />
+                <TextField onChange={(e) => setLname(e.target.value)} value={lname} className="addLead_input" type="text" id="lname" label="lastname" placeholder="Your last name.." />
+                <TextField onChange={(e) => setEmail(e.target.value)} value={email} className="addLead_input" type="email" id="Email" label="Email" placeholder="Your address.." />
+                <TextField onChange={(e) => setAddress(e.target.value)} value={address} className="addLead_input" type="text" id="Address" label="Address" placeholder="Address" />
+                <TextField onChange={(e) => setContact(e.target.value)} value={contact} className="addLead_input" type="number" id="contactNo" label="contactNo" placeholder="contactNo" />
+                <TextField onChange={(e) => setIntrest(e.target.value)} value={intrest} className="addLead_input" type="text" id="intrest" label="intrest" />
+                <TextField onChange={(e) => setStatus(e.target.value)} value={status} className="addLead_input" type="text" id="status" label="status" placeholder="status" />
 
 
                 <button
